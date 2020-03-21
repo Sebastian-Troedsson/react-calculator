f8d19a12dc5619b9850e600668b74e52d089d18f
Sebastian Troedsson <sebastiantroedsson@MacBook-Pro-som-tillhor-Sebastian.local>
2020-03-21 17:06:09 +0100

    added styling

--- src/components/App.js
+++ src/components/App.js
@@ -1,41 +1,22 @@
 import React, { useState } from 'react';
 import Display from './Display';
 import ButtonContainer from './ButtonContainer';
+import { evaluate } from 'mathjs';
 import './App.css';
 
 function App() {
   const [screenValue, setScreenValue] = useState('');
 
   const calculate = () => {
-    if (/^\d+[-+*/]\d+$/.test(screenValue)) {
-      const a = parseInt(screenValue.match(/^\d+/)[0]);
-      const b = parseInt(screenValue.match(/\d+$/)[0]);
-      const operator = screenValue.match(/[-+*/]/)[0];
-
-      switch (operator) {
-        case '+':
-          setScreenValue((a + b).toString());
-          break;
-        case '-':
-          setScreenValue((a - b).toString());
-          break;
-        case '*':
-          setScreenValue((a * b).toString());
-          break;
-        case '/':
-          setScreenValue((a / b).toString());
-          break;
-        default:
-          setScreenValue('');
-          break;
-      };
-    } else {
-      /* Error Handling */
-    };
+    try {
+      setScreenValue(evaluate(screenValue).toString());
+    } catch (error) {
+      console.log('invalid expression');
+    }
   };
 
   return (
-    <div className="App">
+    <div className="app">
       <Display screenValue={screenValue} setScreenValue={setScreenValue}/>
       <ButtonContainer setScreenValue={setScreenValue} calculate={calculate}/>
     </div>
--- src/components/ButtonContainer.js
+++ src/components/ButtonContainer.js
@@ -3,22 +3,25 @@ import React from 'react'
 function ButtonContainer({ setScreenValue, calculate }) {
   return (
     <div className="button-container">
-      <button onClick={() => setScreenValue(prevState => prevState + 9)}>9</button>
-      <button onClick={() => setScreenValue(prevState => prevState + 8)}>8</button>
-      <button onClick={() => setScreenValue(prevState => prevState + 7)}>7</button>
-      <button onClick={() => setScreenValue(prevState => prevState + 6)}>6</button>
-      <button onClick={() => setScreenValue(prevState => prevState + 5)}>5</button>
-      <button onClick={() => setScreenValue(prevState => prevState + 4)}>4</button>
-      <button onClick={() => setScreenValue(prevState => prevState + 3)}>3</button>
-      <button onClick={() => setScreenValue(prevState => prevState + 2)}>2</button>
-      <button onClick={() => setScreenValue(prevState => prevState + 1)}>1</button>
-      <button onClick={() => setScreenValue(prevState => prevState + 0)}>0</button>
-      <button onClick={() => setScreenValue(prevState => prevState + '+')}>+</button>
-      <button onClick={() => setScreenValue(prevState => prevState + '-')}>-</button>
-      <button onClick={() => setScreenValue(prevState => prevState + '*')}>*</button>
-      <button onClick={() => setScreenValue(prevState => prevState + '/')}>/</button>
-      <button onClick={() => calculate()}>=</button>
-      <button onClick={() => setScreenValue('')}>C</button>
+      <button className="color-gray" onClick={() => setScreenValue('')}>C</button>
+      <button className="color-gray" onClick={() => setScreenValue('')}>+/-</button>
+      <button className="color-gray" onClick={() => setScreenValue('')}>%</button>
+      <button className="color-orange" onClick={() => setScreenValue(prevState => prevState + '/')}>/</button>
+      <button className="color-dark" onClick={() => setScreenValue(prevState => prevState + 7)}>7</button>
+      <button className="color-dark" onClick={() => setScreenValue(prevState => prevState + 8)}>8</button>
+      <button className="color-dark" onClick={() => setScreenValue(prevState => prevState + 9)}>9</button>
+      <button className="color-orange" onClick={() => setScreenValue(prevState => prevState + '*')}>X</button>
+      <button className="color-dark" onClick={() => setScreenValue(prevState => prevState + 4)}>4</button>
+      <button className="color-dark" onClick={() => setScreenValue(prevState => prevState + 5)}>5</button>
+      <button className="color-dark" onClick={() => setScreenValue(prevState => prevState + 6)}>6</button>
+      <button className="color-orange" onClick={() => setScreenValue(prevState => prevState + '-')}>-</button>
+      <button className="color-dark" onClick={() => setScreenValue(prevState => prevState + 1)}>1</button>
+      <button className="color-dark" onClick={() => setScreenValue(prevState => prevState + 2)}>2</button>
+      <button className="color-dark" onClick={() => setScreenValue(prevState => prevState + 3)}>3</button>
+      <button className="color-orange" onClick={() => setScreenValue(prevState => prevState + '+')}>+</button>
+      <button className="zero-button color-dark"onClick={() => setScreenValue(prevState => prevState + 0)}>0</button>
+      <button className="color-dark" onClick={() => setScreenValue('.')}>.</button>
+      <button className="color-orange" onClick={() => calculate()}>=</button>
     </div>
   )
 }
